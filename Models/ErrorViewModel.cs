436a584cede6c091a5badac7db7b7dd9cbbb23a3
Jaggesher Mondal <jaggesher14@gmail.com>
2018-02-23 23:09:31 +0600

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineAddressBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
